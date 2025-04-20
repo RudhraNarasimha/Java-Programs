02a7f38f2a719fc36e4b59032eb5d5c07e1294b0
user <user@DESKTOP-OC2FCPH.bbrouter>
2025-04-20 08:38:20 +0530

    Prime Number

---
+++ Basics/src/com/datatypes/practice/PrimeNumbers.java
@@ -0,0 +1,26 @@
+package com.datatypes.practice;
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter Number :");
+
+		int a = scanner.nextInt();
+		
+
+		if (a % a == 0 || a % 1 == 0) {
+
+			System.out.println("Given Number is Prime Number");
+
+		} else {
+			System.out.println("Given Number is not Prime Number");
+		}
+	}
+
+}
